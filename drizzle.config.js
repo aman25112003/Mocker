1d47046a1467e7fad8e0e9d3466d06c27102bedd
AMAN SINGH <amansingh20032511@gmail.com>
2024-10-23 10:27:24 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://Mocker_owner:d9ZYGykg6COj@ep-orange-shape-a1sxzpm5.ap-southeast-1.aws.neon.tech/Mocker?sslmode=require",
+    url: "",
   },
 };
